c5f117e8119993ab6647ef0dbf5d91792c7a4479
alexomaset <alexomaset711@gmail.com>
2019-06-03 16:28:13 +0300

    modified urls.py to link to template files

---
+++ hood/urls.py
@@ -0,0 +1,6 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
