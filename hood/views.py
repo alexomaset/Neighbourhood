d21c0c26b21478beabc0a45ecdca2b738ae9a8b0
alexomaset <alexomaset711@gmail.com>
2019-06-03 16:22:16 +0300

    modified views.py

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import *
