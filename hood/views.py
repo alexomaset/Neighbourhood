722c516a9a9b640bb8f2c4ce49605d417f45de9c
alexomaset <alexomaset711@gmail.com>
2019-06-03 16:24:13 +0300

    modified views.py to include index page

--- hood/views.py
+++ hood/views.py
@@ -11,3 +11,21 @@ from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
 from django.contrib import messages
+
+# views here
+def home(request):
+
+    if request.user.is_authenticated:
+        if Join.objects.filter(user_id=request.user).exists():
+            hood = Hood.objects.get(pk=request.user.join.hood_id.id)
+            posts = Posts.objects.filter(hood=request.user.join.hood_id.id)
+            businesses = Business.objects.filter(
+                hood=request.user.join.hood_id.id)
+
+            return render(request, 'hoods/hood.html', {"hood": hood, "businesses": businesses, "posts": posts})
+        else:
+            neighbourhoods = Hood.objects.all()
+            return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
+    else:
+        neighbourhoods = Hood.objects.all()
+        return render(request, 'index.html', {"neighbourhoods": neighbourhoods})
