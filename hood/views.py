8c11169f33c1802126e860f63cd759b37e0ca663
alexomaset <alexomaset711@gmail.com>
2019-06-03 16:22:50 +0300

    modified views.py

--- hood/views.py
+++ hood/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect, JsonRespons
 from django.contrib.auth.decorators import login_required
 from .models import *
 from .forms import *
+from django.contrib.auth import login, authenticate
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.template.loader import render_to_string
+from django.contrib.auth.models import User
+from django.core.mail import EmailMessage
+from django.contrib import messages
